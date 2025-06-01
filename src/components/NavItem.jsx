d892aae74f3a5b543710a0116298c46facdb2330
JordiGis <jorgisfer@alu.edu.gva.es>
2025-06-01 18:26:12 +0200

    ADD paginas

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -6,26 +6,12 @@ const NavItem = ({ path, label }) => {
     const router = useRouter();
     const isActive = router.pathname === path;
 
-    if (isActive) {
-        return (
-            <Link
-                className="navItem active"
-                href="#"
-            >
-                <span >
-                    {label}
-                </span>
-            </Link>
-
-        );
-    }
-
     return (
         <Link
-            className="navItem "
-            href={path}
+            className={`navItem ${isActive ? 'active' : ''}`}
+            href={isActive ? '#' : path}
         >
-            <span >
+            <span>
                 {label}
             </span>
         </Link>
