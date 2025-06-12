14008a1d2646e05b5c457e4fa029704982af43df
JordiGis <jorgisfer@alu.edu.gva.es>
2025-06-12 22:20:53 +0200

    ADD mensaje error conexion con servidor

--- src/utils/interceptedFetch.js
+++ src/utils/interceptedFetch.js
@@ -1,5 +1,6 @@
 // utils/interceptedFetch.ts
 import { saveToken, gatTokenByHeaderRequest } from '@/utils/auth/token';
+import Swal from 'sweetalert2';
 
 export function patchGlobalFetch(token, updateToken) {
     const originalFetch = window.fetch;
@@ -11,15 +12,36 @@ export function patchGlobalFetch(token, updateToken) {
         }
 
         init.headers = headers;
+        try {
+            const response = await originalFetch(input, init);
+            const newToken = gatTokenByHeaderRequest(response);
+            if (newToken) {
+                saveToken(newToken);
+                updateToken(newToken);
+            }
+            return response;
 
-        const response = await originalFetch(input, init);
+        } catch (error) {
+            if (error instanceof TypeError && error.message === 'Failed to fetch') {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error de conexión',
+                    text: 'No se pudo conectar al servidor.',
+                });
+                return new Response(JSON.stringify({
+                    message: 'No se pudo conectar al servidor. Por favor, inténtelo más tarde.'
+                }), {
+                    status: 503,
+                    ok: false,
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
 
-        const newToken = gatTokenByHeaderRequest(response);
-        if (newToken) {
-            saveToken(newToken);
-            updateToken(newToken); // <--- actualiza el contexto
+            } else {
+                console.error('Fetch error:', error);
+                throw error; // Re-throw the error to handle it later
+            }
         }
-
-        return response;
     };
 }
