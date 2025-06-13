0f4b84306f9140a7dee8539c938fcf2a0d4297ad
JordiGis <jorgisfer@alu.edu.gva.es>
2025-06-13 20:25:51 +0200

    ADD paginacion

--- src/pages/search/index.jsx
+++ src/pages/search/index.jsx
@@ -1,236 +1,128 @@
+'use client'
 import React, { useEffect, useState } from 'react';
 import styles from './style.module.css';
 import stylesCard from './card.module.css';
+import Swal from 'sweetalert2';
+const api = process.env.NEXT_PUBLIC_API_URL;
 
+function Paginate({ paginate, setPage, page }) {
+    const handlePageChange = (pageNumber) => {
+        if (pageNumber < 1 || pageNumber > paginate.totalPages) return;
+        setPage(pageNumber);
+    };
+
+    const pageNumbers = [];
+    for (let i = 0; i < 5; i++) {
+        const pageNumber = page + i;
+        if (pageNumber <= paginate.totalPages) {
+            pageNumbers.push(pageNumber);
+        }
+    }
+
+    return (
+        <div className={styles.paginate}>
+            <button
+                className={styles.prevButton}
+                onClick={() => handlePageChange(page - 1)}
+                disabled={page === 1}
+            >
+                Anterior
+            </button>
+
+            <span className={styles.pageInfo}>
+                Página {page} de {paginate.totalPages}
+            </span>
+            {/* <div>
+                {pageNumbers.map((num) => (
+                    <button
+                        key={num}
+                        className={`${styles.pageButton} ${num === page ? styles.activePage : ''}`}
+                        onClick={() => handlePageChange(num)}
+                    >
+                        {num}
+                    </button>
+                ))}
+            </div> */}
+            <button
+                className={styles.nextButton}
+                onClick={() => handlePageChange(page + 1)}
+                disabled={page === paginate.totalPages}
+            >
+                Siguiente
+            </button>
+        </div>
+    );
+}
 
 
 function SearchPage() {
-    const data = [
-        {
-            "id": 3,
-            "nombre": "Carlos",
-            "apellidos": "Ruiz Fernández",
-            "username": "carlos.ruiz",
-            "email": "carlos@example.com",
-            "password": "$2b$10$jO8k3lPvM3HjvKY/yG6fAuW6T1b9BGtfxXy2QYLrpvZUwhGnFaFEC",
-            "pueblo": "Valencia",
-            "gradoId": 4,
-            "ramaId": 1,
-            "estado": "conectado",
-            "fotoPerfil": "https://ui-avatars.com/api/?uppercase=false&name=Carlos+Ruiz Fernández",
-            "descripcion": "Desde Valencia, apasionado por el mundo del software, el desarrollo y las nuevas tecnologías. Siempre conectado y en constante aprendizaje, Carlos destaca por su compromiso y curiosidad para enfrentarse a nuevos retos de programación. Su perfil combina la solidez técnica con una gran capacidad de adaptación, preparado para abordar proyectos tanto académicos como profesionales. 🚀",
-            "telefono": null,
-            "ultimaConexion": "2025-06-10T19:39:46.021Z",
-            "rolId": null,
-            "empresaId": null,
-            "buscaEmpresa": false,
-            "visibilidad": true,
-            "creadoEn": "2025-06-10T19:39:46.021Z",
-            "grado": {
-                "id": 4,
-                "nombre": "2 DAM"
-            },
-            "rama": {
-                "id": 1,
-                "nombre": "Informática"
-            },
-            "borrado": false
-        },
-        {
-            "id": 3,
-            "nombre": "Carlos",
-            "apellidos": "Ruiz Fernández",
-            "username": "carlos.ruiz",
-            "email": "carlos@example.com",
-            "password": "$2b$10$jO8k3lPvM3HjvKY/yG6fAuW6T1b9BGtfxXy2QYLrpvZUwhGnFaFEC",
-            "pueblo": "Valencia",
-            "gradoId": 4,
-            "ramaId": 1,
-            "estado": "conectado",
-            "fotoPerfil": "https://ui-avatars.com/api/?uppercase=false&name=Carlos+Ruiz Fernández",
-            "descripcion": "Desde Valencia, apasionado por el mundo del software, el desarrollo y las nuevas tecnologías. Siempre conectado y en constante aprendizaje, Carlos destaca por su compromiso y curiosidad para enfrentarse a nuevos retos de programación. Su perfil combina la solidez técnica con una gran capacidad de adaptación, preparado para abordar proyectos tanto académicos como profesionales. 🚀",
-            "telefono": null,
-            "ultimaConexion": "2025-06-10T19:39:46.021Z",
-            "rolId": null,
-            "empresaId": null,
-            "buscaEmpresa": false,
-            "visibilidad": true,
-            "creadoEn": "2025-06-10T19:39:46.021Z",
-            "grado": {
-                "id": 4,
-                "nombre": "2 DAM"
-            },
-            "rama": {
-                "id": 1,
-                "nombre": "Informática"
-            },
-            "borrado": false
-        },
-        {
-            "id": 3,
-            "nombre": "Carlos",
-            "apellidos": "Ruiz Fernández",
-            "username": "carlos.ruiz",
-            "email": "carlos@example.com",
-            "password": "$2b$10$jO8k3lPvM3HjvKY/yG6fAuW6T1b9BGtfxXy2QYLrpvZUwhGnFaFEC",
-            "pueblo": "Valencia",
-            "gradoId": 4,
-            "ramaId": 1,
-            "estado": "conectado",
-            "fotoPerfil": "https://ui-avatars.com/api/?uppercase=false&name=Carlos+Ruiz Fernández",
-            "descripcion": "Desde Valencia, apasionado por el mundo del software, el desarrollo y las nuevas tecnologías. Siempre conectado y en constante aprendizaje, Carlos destaca por su compromiso y curiosidad para enfrentarse a nuevos retos de programación. Su perfil combina la solidez técnica con una gran capacidad de adaptación, preparado para abordar proyectos tanto académicos como profesionales. 🚀",
-            "telefono": null,
-            "ultimaConexion": "2025-06-10T19:39:46.021Z",
-            "rolId": null,
-            "empresaId": null,
-            "buscaEmpresa": false,
-            "visibilidad": true,
-            "creadoEn": "2025-06-10T19:39:46.021Z",
-            "grado": {
-                "id": 4,
-                "nombre": "2 DAM"
-            },
-            "rama": {
-                "id": 1,
-                "nombre": "Informática"
-            },
-            "borrado": false
-        },
-        {
-            "id": 3,
-            "nombre": "Carlos",
-            "apellidos": "Ruiz Fernández",
-            "username": "carlos.ruiz",
-            "email": "carlos@example.com",
-            "password": "$2b$10$jO8k3lPvM3HjvKY/yG6fAuW6T1b9BGtfxXy2QYLrpvZUwhGnFaFEC",
-            "pueblo": "Valencia",
-            "gradoId": 4,
-            "ramaId": 1,
-            "estado": "conectado",
-            "fotoPerfil": "https://ui-avatars.com/api/?uppercase=false&name=Carlos+Ruiz Fernández",
-            "descripcion": "Desde Valencia, apasionado por el mundo del software, el desarrollo y las nuevas tecnologías. Siempre conectado y en constante aprendizaje, Carlos destaca por su compromiso y curiosidad para enfrentarse a nuevos retos de programación. Su perfil combina la solidez técnica con una gran capacidad de adaptación, preparado para abordar proyectos tanto académicos como profesionales. 🚀",
-            "telefono": null,
-            "ultimaConexion": "2025-06-10T19:39:46.021Z",
-            "rolId": null,
-            "empresaId": null,
-            "buscaEmpresa": false,
-            "visibilidad": true,
-            "creadoEn": "2025-06-10T19:39:46.021Z",
-            "grado": {
-                "id": 4,
-                "nombre": "2 DAM"
-            },
-            "rama": {
-                "id": 1,
-                "nombre": "Informática"
-            },
-            "borrado": false
-        },
-        {
-            "id": 3,
-            "nombre": "Carlos",
-            "apellidos": "Ruiz Fernández",
-            "username": "carlos.ruiz",
-            "email": "carlos@example.com",
-            "password": "$2b$10$jO8k3lPvM3HjvKY/yG6fAuW6T1b9BGtfxXy2QYLrpvZUwhGnFaFEC",
-            "pueblo": "Valencia",
-            "gradoId": 4,
-            "ramaId": 1,
-            "estado": "conectado",
-            "fotoPerfil": "https://ui-avatars.com/api/?uppercase=false&name=Carlos+Ruiz Fernández",
-            "descripcion": "Desde Valencia, apasionado por el mundo del software, el desarrollo y las nuevas tecnologías. Siempre conectado y en constante aprendizaje, Carlos destaca por su compromiso y curiosidad para enfrentarse a nuevos retos de programación. Su perfil combina la solidez técnica con una gran capacidad de adaptación, preparado para abordar proyectos tanto académicos como profesionales. 🚀",
-            "telefono": null,
-            "ultimaConexion": "2025-06-10T19:39:46.021Z",
-            "rolId": null,
-            "empresaId": null,
-            "buscaEmpresa": false,
-            "visibilidad": true,
-            "creadoEn": "2025-06-10T19:39:46.021Z",
-            "grado": {
-                "id": 4,
-                "nombre": "2 DAM"
-            },
-            "rama": {
-                "id": 1,
-                "nombre": "Informática"
-            },
-            "borrado": false
-        },
-        {
-            "id": 3,
-            "nombre": "Carlos",
-            "apellidos": "Ruiz Fernández",
-            "username": "carlos.ruiz",
-            "email": "carlos@example.com",
-            "password": "$2b$10$jO8k3lPvM3HjvKY/yG6fAuW6T1b9BGtfxXy2QYLrpvZUwhGnFaFEC",
-            "pueblo": "Valencia",
-            "gradoId": 4,
-            "ramaId": 1,
-            "estado": "conectado",
-            "fotoPerfil": "https://ui-avatars.com/api/?uppercase=false&name=Carlos+Ruiz Fernández",
-            "descripcion": "Desde Valencia, apasionado por el mundo del software, el desarrollo y las nuevas tecnologías. Siempre conectado y en constante aprendizaje, Carlos destaca por su compromiso y curiosidad para enfrentarse a nuevos retos de programación. Su perfil combina la solidez técnica con una gran capacidad de adaptación, preparado para abordar proyectos tanto académicos como profesionales. 🚀",
-            "telefono": null,
-            "ultimaConexion": "2025-06-10T19:39:46.021Z",
-            "rolId": null,
-            "empresaId": null,
-            "buscaEmpresa": false,
-            "visibilidad": true,
-            "creadoEn": "2025-06-10T19:39:46.021Z",
-            "grado": {
-                "id": 4,
-                "nombre": "2 DAM"
-            },
-            "rama": {
-                "id": 1,
-                "nombre": "Informática"
-            },
-            "borrado": false
-        },
-        {
-            "id": 3,
-            "nombre": "Carlos",
-            "apellidos": "Ruiz Fernández",
-            "username": "carlos.ruiz",
-            "email": "carlos@example.com",
-            "password": "$2b$10$jO8k3lPvM3HjvKY/yG6fAuW6T1b9BGtfxXy2QYLrpvZUwhGnFaFEC",
-            "pueblo": "Valencia",
-            "gradoId": 4,
-            "ramaId": 1,
-            "estado": "conectado",
-            "fotoPerfil": "https://ui-avatars.com/api/?uppercase=false&name=Carlos+Ruiz Fernández",
-            "descripcion": "Desde Valencia, apasionado por el mundo del software, el desarrollo y las nuevas tecnologías. Siempre conectado y en constante aprendizaje, Carlos destaca por su compromiso y curiosidad para enfrentarse a nuevos retos de programación. Su perfil combina la solidez técnica con una gran capacidad de adaptación, preparado para abordar proyectos tanto académicos como profesionales. 🚀",
-            "telefono": null,
-            "ultimaConexion": "2025-06-10T19:39:46.021Z",
-            "rolId": null,
-            "empresaId": null,
-            "buscaEmpresa": false,
-            "visibilidad": true,
-            "creadoEn": "2025-06-10T19:39:46.021Z",
-            "grado": {
-                "id": 4,
-                "nombre": "2 DAM"
-            },
-            "rama": {
-                "id": 1,
-                "nombre": "Informática"
-            },
-            "borrado": false
-        },
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1);
+    const [paginate, setPaginate] = useState({
+        totalPages: 1,
+        totalItems: 0,
+        limit: 10,
+        currentPage: 1,
+    });
 
-    ]
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await fetch(`${api}users/filter/?page=${page}&limit=10`);
+                const res = await response.json();
 
-    const openProfile = (id) => {
+                if (res) {
+                    setData(res.data);
+                    setPaginate(res.pagination);
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, [page]);
+
+    const openProfile = async (id) => {
         console.log(`Abrir perfil del alumno con ID: ${id}`);
+        const card = document.getElementById(`${id}`);
+        if (card) {
+            card.classList.add(stylesCard.active);
+        }
+        await Swal.fire({
+            title: 'Perfil del Alumno',
+            text: `ID del Alumno: ${id}`,
+            icon: 'info',
+            confirmButtonText: 'Aceptar'
+        }).finally(() => {
+            setTimeout(() => {
+                card.classList.remove(stylesCard.active);
+            }, 3000);
+        });
+    };
+
+    if (loading) {
+        return <div className={styles.loading}>Cargando...</div>;
+    }
+
+    if (!data || data.length === 0) {
+        return <div className={styles.noData}>No hay alumnos disponibles</div>;
     }
 
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>Buscar Alumnos</h1>
-
+            <Paginate paginate={paginate} setPage={setPage} page={page} />
             <div className={styles.cards}>
                 {data.map((item) => (
-                    <div key={item.id} className={stylesCard.card}>
-                        <div className={stylesCard.buttons} onClick={() => openProfile(item.id)}><button type='button' className={stylesCard.see}><i className="bi bi-eyeglasses"></i></button></div>
+                    <div key={item.id} id={item.id} className={`${stylesCard.card}`}>
+                        <div className={stylesCard.buttons} onClick={() => openProfile(item.id)}>
+                            <button type='button' className={stylesCard.see}>
+                                <i className="bi bi-eyeglasses"></i>
+                            </button>
+                        </div>
                         <img src={item.fotoPerfil} alt={`${item.nombre} ${item.apellidos}`} className={stylesCard.image} />
                         <h2 className={stylesCard.name}>{item.nombre} {item.apellidos}</h2>
                         <p className={stylesCard.username}>@{item.username}</p>
@@ -238,12 +130,12 @@ function SearchPage() {
                         <p className={stylesCard.pueblo}>{item.pueblo}</p>
                         <p className={stylesCard.grado}>Grado: {item.grado.nombre}</p>
                         <p className={stylesCard.rama}>Rama: {item.rama.nombre}</p>
-                        <p className={stylesCard.descripcion}>{item.descripcion}</p>
+                        {item.descripcion && <p className={stylesCard.descripcion}>{item.descripcion}</p>}
                     </div>
                 ))}
             </div>
+            <Paginate paginate={paginate} setPage={setPage} page={page} />
         </div>
-
     );
 }
 
